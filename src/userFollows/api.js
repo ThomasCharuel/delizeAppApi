0e1eec6143a99439a863f1d67478e3912b29f17a
Thomas <thomascharuel@hotmail.fr>
2018-02-04 16:38:26 +0100

    follows entities

---
+++ src/userFollows/api.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const model = require('./model');
+
+const router = express.Router();
+
+// Automatically parse request body as JSON
+router.use(bodyParser.json());
+
+/**
+ * GET /userFollows
+ *
+ * Retrieve userFollows
+ */
+router.get('/', (req, res, next) => {
+  model.list((err, entities) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json( entities );
+  });
+});
+
+
+/**
+ * POST /userFollows
+ *
+ * Create a new userFollow.
+ */
+router.post('/', (req, res, next) => {
+  model.create(req.body, (err, entity) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(entity);
+  });
+});
+
+/**
+ * GET /userFollows/:id
+ *
+ * Retrieve an userFollow.
+ */
+router.get('/:userFollow', (req, res, next) => {
+  model.read(req.params.userFollow, (err, entity) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(entity);
+  });
+});
+
+
+
+/**
+ * Errors on "/userFollows/*" routes.
+ */
+router.use((err, req, res, next) => {
+  // Format error and forward to generic error handler for logging and
+  // responding to the request
+  err.response = {
+    message: err.message,
+    internalCode: err.code
+  };
+  next(err);
+});
+
+module.exports = router;
